f6d111cd19f2ddf9643236107e3f4be10572aa50
Megan Walford <mwalford22@gmail.com>
2023-11-10 14:01:47 +0000

    Addition of new recipe and confirmation functions

--- run.py
+++ run.py
@@ -12,7 +12,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project_three')
 
-vault = SHEET.worksheet('vault')
+def add_new_recipe():
+    """
+    Start command for adding a recipe to the Vault worksheet
+    """
+    print("What is the name of your recipe?")
+    recipe_name = input(" \n")
+    print(f"New recipe is called: {recipe_name}\n")
+    print("Is this correct? Yes/No ")
+    user_answer = input().lower()
+    return user_answer
 
-data = vault.get_all_values()
-print(data)
+def confirmation():
+    """
+    Create a loop to confirm if yes or no has been typed
+    """
+    while True:
+        user_answer = add_new_recipe()
+        if user_answer == "yes":
+            break
+        else:
+            print("----")
+
+    
+def pull_vault_data():
+    """
+    Retrieve data from the vault worksheet
+    """
+
+add_new_recipe()
+confirmation()
